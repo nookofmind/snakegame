59488f4327a1ddfab4d24094a7fae94b9198ddc0
nookofmind <juszczak.dorota@icloud.com>
2022-08-31 16:30:02 +0200

    Added food

--- Snake.py
+++ Snake.py
@@ -3,26 +3,38 @@ import random
 import time
 from pygame import mixer
 
+# Defining colors
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+lavender = pygame.Color(235, 150, 235)
+magenta = pygame.Color(224, 81, 148)
+salmon = pygame.Color(255, 141, 133)
+
+# Defining window size
+window_x = 500
+window_y = 500
+
 # Snake class
 class snakeClass:
     def __init__(self):
         self.snakeStartingLenght = 1
-        self.snakeStartingBodyPositions = [[0,0]]
+        self.snakeStartingBodyPositions = [[0, 0]]
         self.snakeDirection = 'right'
         self.desiredDirection = 'none'
         self.snakePartSize = 20
+        self.snakeSpeed = 10
 
     # Move snake
     def moveSnake(self):
         for snakePart in self.snakeStartingBodyPositions:
-           if self.snakeDirection == 'right':
-               snakePart[0] = snakePart[0] + self.snakePartSize
-           elif self.snakeDirection == 'left':
-               snakePart[0] = snakePart[0] - self.snakePartSize
-           elif self.snakeDirection == 'up':
-               snakePart[1] = snakePart[1] - self.snakePartSize
-           elif self.snakeDirection == 'down':
-               snakePart[1] = snakePart[1] + self.snakePartSize
+            if self.snakeDirection == 'right':
+                snakePart[0] = snakePart[0] + self.snakePartSize
+            elif self.snakeDirection == 'left':
+                snakePart[0] = snakePart[0] - self.snakePartSize
+            elif self.snakeDirection == 'up':
+                snakePart[1] = snakePart[1] - self.snakePartSize
+            elif self.snakeDirection == 'down':
+                snakePart[1] = snakePart[1] + self.snakePartSize
 
     # Change direction
     def changeDirection(self):
@@ -35,37 +47,29 @@ class snakeClass:
         elif self.desiredDirection == 'down' and self.snakeDirection != 'up':
             self.snakeDirection = self.desiredDirection
 
-# Game over
-def game_over():
 
-    # Quit the window after 2 seconds
-    time.sleep(2)
+# Food class
+class foodClass:
 
-     # Deactivating pygame library
-    pygame.quit()
+    def __init__(self):
+        self.foodSize = 20
+        self.foodSpawn = True
+        self.foodPosition = [random.randrange(1, (window_x//20)) * 20,
+                  random.randrange(1, (window_y//20)) * 20]
 
-     # Quit the program
-    quit()
-
-# Initial score
-score = 0
-
-# Defining colors
-black = pygame.Color(0, 0, 0)
-white = pygame.Color(255, 255, 255)
-lavender = pygame.Color(235, 150, 235)
-magenta = pygame.Color(224, 81, 148)
-salmon = pygame.Color(255, 141, 133)
-
-# Defining snake speed
-snake_speed = 10
+    def addFood(self):
+        pygame.draw.rect(game_window, magenta, pygame.Rect(
+            self.foodPosition[0], self.foodPosition[1], self.foodSize, self.foodSize))
 
 # Create snake instance
 Snake = snakeClass()
+Food = foodClass()
 
-# Window size
-window_x = 500
-window_y = 500
+# Game over
+def game_over():
+    time.sleep(2)
+    pygame.quit()
+    quit()
 
 # Initialising pygame
 pygame.init()
@@ -77,11 +81,14 @@ game_window = pygame.display.set_mode((window_x, window_y))
 # Display window
 pygame.display.flip()
 
-# Play music
+# Play Dora song
 mixer.init()
 mixer.music.load("song.mp3")
 mixer.music.set_volume(0.2)
 mixer.music.play()
+
+# Initial score
+score = 0
 
 # Loop - play the game
 while True:
@@ -101,21 +108,23 @@ while True:
     Snake.changeDirection()
 
     # Draw window
-    game_window.fill(magenta)
+    game_window.fill(lavender)
 
     # Draw snake
     for snakePart in Snake.snakeStartingBodyPositions:
         pygame.draw.rect(game_window, white, pygame.Rect(
             snakePart[0], snakePart[1], Snake.snakePartSize, Snake.snakePartSize))
 
-# Refresh game screen
+    # Add food
+    Food.addFood()
+
+    # Refresh game screen
     pygame.display.update()
 
-# Move snake
+    # Move snake
     Snake.moveSnake()
     time.sleep(0.1)
 
-# Game over - touching the edge of the screen
+    # Game over - touching the edge of the screen
     if snakePart[0] >= window_x or snakePart[0] < 0 or snakePart[1] >= window_y or snakePart[1] < 0:
         game_over()
-
